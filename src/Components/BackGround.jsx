19f4d5d45a22241ccf92358d6c24753faf0b2a4a
bettytadege <bettytadege13@gmail.com>
2024-08-07 23:07:36 +0300

    components

---
+++ src/Components/BackGround.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+
+function BackGround() {
+
+  return (
+    <div>BackGround</div>
+  )
+}
+
+export default BackGround
---
+++ src/Components/MiniCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState } from 'react'
+
+function MiniCard() {
+    
+  return (
+    <div>MiniCard</div>
+  )
+}
+
+export default MiniCard
