fe28842123105613276cf8648bb0a7339cef5f8e
bettytadege <bettytadege13@gmail.com>
2024-08-07 23:11:56 +0300

    useDate hook

---
+++ src/utils/useDate.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react"
+
+export const useDate=()=>{
+    const locale='en'
+    const[today,setDate]=useState(new Date())
+
+    useEffect(()=>{
+        const timer=setInterval(()=>{
+            setDate(new Date())
+        },60 * 1000)
+    },[])
+ const day=today.toLocaleDateString(locale,{weekday:'long'})
+ const date=   `${day},${today.getDate()},${today.toLocaleDateString(locale,{month:'long'})}\n`
+ const time=today.toLocaleDateString(locale,{hour:'numeric',hour12:true,minute:'numeric'})
+
+ 
+return{
+    date,time
+}
+
+}
